82aafd64e4d6b70e2e2d74115569b69a780db42a
HemanthChirikonda <67725821+HemanthChirikonda@users.noreply.github.com>
2024-07-15 16:01:43 -0400

    jaav proect, datatypes

---
+++ src/main/java/com/learning/day1/DataTypes.java
@@ -0,0 +1,61 @@
+package com.learning.day1;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+        // DataTypes are divided into two groups :
+//        1. Primitive Data types        => Data
+//        2. Non - Primitive Data Types. => Data and Actions
+
+        // Primitive DataTypes :
+//         name                           memory                      range
+//        1. byte                          1byte              range is -128  to 127
+//        2. short                         2Bytes             range is -32,768 to 32,767.
+//        3. int                           4bytes             range is -2,147,483,648 to 2,147,483,647
+//        4. long                          8 bytes            range is -9,223,372,036,854,775,808  to 9,223,372,036,854,775,807
+//        5. float                         4 bytes            its is used to store fractional numbers, it can store up to 6 or 7 decimal digits  ex: 12.4567889
+//        6. double                        8 bytes            its is used to store fractional numbers, it can store up to  15 decimal digits     ex : 12.123456789987654
+//        7. boolean                       1 bit (0,1)        0 -> false, 1-> true
+//        8. char                          2 bytes        a-z, A-Z, 0-9, other symbols,   for char we use '' (single quotes)
+
+
+//        All the variable names must start with lower case.
+//        This is   the way to create variable:
+
+//        variableDatatype  variableNAme = variableValue;
+
+               byte  a = 100;
+
+               short b =1000;
+
+               int c = 1;
+
+               float d = 10.45678f;    // for float, we need to end with f to represents its float type.
+
+               long e = 1;
+
+               double f = 12.5;
+
+               char g = 'A';
+
+
+
+           // Non-Primitive Datatypes :
+          //                        - String - Class   - we use "" (double Quotes)
+
+          String h = "hello";
+
+
+       System.out.println(d);
+
+
+
+
+
+
+
+
+
+    }
+}
